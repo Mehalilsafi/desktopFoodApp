754c5b5de0dd2c626b8a465ca1d8b9d973157c12
safi <safiou102003@gmail.com>
2023-12-13 13:33:16 -0800

    first commit

---
+++ src/main/java/com/mycompany/foodapplication/Foodapplication.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.foodapplication;
+
+/**
+ *
+ * @author hy
+ */
+public class Foodapplication {
+
+    public static void main(String[] args) {
+    split splitFrame = new split();
+
+        // Set the default close operation (close the application when the frame is closed)
+        splitFrame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        // Make the frame visible
+        splitFrame.setVisible(true);
+
+        // You can also add other initialization or configuration code here if needed
+
+        // Print a message to the console
+        System.out.println("Hello World!");
+    }
+}
